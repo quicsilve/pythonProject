c32eaf2d02b1525c227f1e1d00fde9c6179ec236
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-09-24 21:23:29 +0300

    4 Lesson

---
+++ 4 Lesson.py
@@ -0,0 +1,5 @@
+number_of_completed_task = 12 # Количество выполненных ДЗ
+number_of_hours_spent = 1.5 # Количество затраченных часов
+course_title = 'Python' # Название курса
+
+print(course_title, 'Всего задач: ',number_of_completed_task, 'Затрачено часов: ', number_of_hours_spent, 'Среднее время выполнения: ', number_of_completed_task / number_of_hours_spent) # вывод всего по порядку
