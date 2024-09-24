2edb51f481aa946801b2cea5f47bf19cbc413123
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-09-24 11:48:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print('task1')
+
+print(9**0.5*5)
+
+
+print('task2')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+
+print('task3')
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+
+print('task4')
+print((float('123.456'))*10)
