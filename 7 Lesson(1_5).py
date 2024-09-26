6eec4a81f412b5a118b91776913c2fee3c01abd2
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-09-26 20:53:26 +0300

    7 Lesson

---
+++ 7 Lesson(1_5).py
@@ -0,0 +1,10 @@
+immutable_var = 21, "Июнь", bool # 1 задача
+print(immutable_var)
+
+#immutable_var[0] = 22 - не сработает т.к это не изменяемый объект // 2 задача
+
+mutable_list = [21, "Июнь", "apple", 2.5] # 3 задача
+print(mutable_list)
+mutable_list[2] = "watermelon"
+print(mutable_list)
+
