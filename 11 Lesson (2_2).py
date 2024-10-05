086b383351632d6f118212edefe479debce08537
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-10-05 17:02:02 +0300

    11L (2_2)

--- 11 Lesson (2_2).py
+++ 11 Lesson (2_2).py
@@ -1,1 +1,12 @@
+numbers1 = int(input("Введите число: "))
+numbers2 = int(input("Введите число: "))
+numbers3 = int(input("Введите число: "))
+if numbers1 == numbers2 == numbers3:
+    print(3)
+
+if numbers1 == numbers2 or numbers1 == numbers3 or numbers2 == numbers3:
+    print(2)
+else:
+    print(0)
+
 
