32d25434db94689527f61c9db08c31c4dda2e239
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-10-17 16:11:03 +0300

    15

--- 15 Lesson (2_5).py
+++ 15 Lesson (2_5).py
@@ -2,9 +2,10 @@ import random
 
 choice = random.randint(3, 20)
 
-sum = 0
+passw = ''
 
-for i in choice:
-    for g in range(i+1, 20):
-        if (sum(i) + sum(g)) % 2 == 0:
-            print(i,g) 
+for i in range(1, choice // 2 + 1):
+    for g in range(i + 1, choice + 1 - i):
+        if choice % (i + g) == 0:
+            passw += str(i) + str(g)
+print(f"Пароль для числа {choice}: {passw}")
