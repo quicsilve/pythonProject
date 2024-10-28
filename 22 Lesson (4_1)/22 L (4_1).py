9b8e80493b254fc460387753e4764ca7a215ad9c
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-10-28 21:48:58 +0300

    22 +

---
+++ 22 Lesson (4_1)/22 L (4_1).py
@@ -0,0 +1,13 @@
+from true_math import divide as dvdtm
+from fake_math import divide as dvdfm
+
+
+result1 = dvdfm(69, 3)
+result2 = dvdfm(3, 0)
+result3 = dvdtm(49, 7)
+result4 = dvdtm(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ 22 Lesson (4_1)/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    resultd = (first/second)
+    return resultd
