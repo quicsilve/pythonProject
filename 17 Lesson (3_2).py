ed3c6d046c5c4ffbb2168418ae49c10c45ebb5b9
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-10-30 21:20:37 +0300

    17 +++

--- 17 Lesson (3_2).py
+++ 17 Lesson (3_2).py
@@ -1,18 +1,22 @@
-def send_email(message, recipient , sender = "university.help@gmail.com"):
+def send_email(message, recipient , * , sender = "university.help@gmail.com"):
 
     if "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")) or "@" not in sender or not sender.endswith((".com", ".ru", ".net")):
         print("Неверный формат мыла", sender, "и", recipient, "не пойдет")
 
+
     elif sender == recipient:
         print("Мыло одинаковое, не пойдет")
 
+
     elif sender == "university.help@gmail.com":
         print("Ура, все прошло как по маслу, отправилось c", sender, "на", recipient)
+
     else:
-        print("Мыло что-ли поменял? Аж не узнал, но письмо равно отправил на", recipient)
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с", sender , "на", recipient)
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
