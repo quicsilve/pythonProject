e2010cf130f3a6672de8720ede98138c2352e5d0
quicsilve <159514061+quicsilve@users.noreply.github.com>
2024-11-05 12:34:36 +0300

    23 - исправления

--- 23 Lesson (Пространство имен).py
+++ 23 Lesson (Пространство имен).py
@@ -10,6 +10,7 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-    inner_function()
+# inner_function()
 
-test_function()
+# NameError: name 'inner_function' is not defined. Did you mean: 'test_function'? -
+# Название ошибки: имя 'inner_function' не определено. Вы имели в виду: 'test_function'?
